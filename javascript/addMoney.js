abd2f8a6ef957df69dfe6dc07dab63ee45c3b212
Mas-rafe <eftemasrafe@gmail.com>
2025-02-28 21:31:04 +0600

    half done

--- javascript/addMoney.js
+++ javascript/addMoney.js
@@ -1,31 +1,40 @@
 document.getElementById("btn-add-money")
 .addEventListener('click',function(event){
     event.preventDefault();
+    const selectBank = document.getElementById("select-bank").value
     const amount = document.getElementById("add-amount").value
-    const addAmount = parseFloat(amount); 
+          const addAmount = parseFloat(amount); 
     const pin = document.getElementById("pin").value
-          const addPin = parseInt(pin);
+          // const addPin = parseInt(pin);
     const balance = document.getElementById("balance")
     .innerText;
           const mainBalance = parseFloat(balance);
+    const accNumber = document.getElementById("account-number").value
+         // const accountNumber = parseInt(acNumber)
           
-    if(addAmount && addPin){
-
-        if( pin.length === 4){
-        const sum = addAmount + mainBalance;
-        document.getElementById("balance").innerText =sum
-        console.log(sum)
-        }
-        else{
-             alert("need valid pin")
-        } 
-    }
-    else{
-        alert("enter amount")
-    }
-
-    window.location.href
+   if(selectBank && accNumber.length === 11){
+       if(addAmount >= 500){
+       
+       
+           if( pin.length === 4){ 
+            const sum = addAmount+ mainBalance;
+            document.getElementById("balance").innerText = sum;
+            console.log(sum)
+               
+               
+   }else {
+       alert("need vlid pin")
+   } 
+       }else {
+           alert("need valid amount")
+   
+       }  
+         }else{
+           alert("need valid bank / number")
+         }
 })
-    
 
-
+     
+      
+     
+     
---
+++ javascript/nothing.js
@@ -0,0 +1,18 @@
+document.getElementById("btn-add-money")
+.addEventListener('click',function(event){
+    event.preventDefault();
+    const accountNumber = document.getElementById("account-number").value
+          //const accountNumber = parseInt(acNumber)
+    const amount = document.getElementById("add-amount").value
+          //const addAmount = parseFloat(amount); 
+    const pin = document.getElementById("pin").value
+         // const addPin = parseInt(pin);
+    const balance = document.getElementById("balance")
+    .innerText;
+          const mainBalance = parseFloat(balance);
+    
+          if(accountNumber === 1234){
+     
+          }
+          
+})
